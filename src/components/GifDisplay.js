54fd8f68c502f7d6372e34dbf232c9ae366dca43
Gleb Posobin <posobin@gmail.com>
2018-12-08 23:33:02 -0500

    Add ability to display gifs anywhere

--- src/components/GifDisplay.js
+++ src/components/GifDisplay.js
@@ -3,11 +3,13 @@ import Gif from './Gif';
 
 class GifDisplay extends React.Component {
   render() {
-    if (!this.props.url) {
+    if (!this.props.gif) {
       return "";
     }
     return (
-      <Gif className="gifPlayer" src={this.props.url} alt="gif"
+      <Gif className="gifPlayer" src={this.props.gif.url} alt="gif"
+        style={{marginLeft: this.props.gif.fracX*100 + '%',
+          marginTop: this.props.gif.fracY*100 + '%'}}
         autoPlay={true} width={150} loop={false} onEnd={this.props.onEnd}
       />
     );
