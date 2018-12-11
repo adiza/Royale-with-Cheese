03ac01cb4efdd19c660edc549ad2747fb4e45706
Gleb Posobin <posobin@gmail.com>
2018-12-11 16:03:53 -0500

    Add production build

---
+++ docs/precache-manifest.38ce5ca689082686814ffa7a3bab7b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ccec6ff05842236dbd2",
+    "url": "/Royale-with-Cheese/static/css/main.20441fd7.chunk.css"
+  },
+  {
+    "revision": "4ccec6ff05842236dbd2",
+    "url": "/Royale-with-Cheese/static/js/main.4ccec6ff.chunk.js"
+  },
+  {
+    "revision": "68a43dc7798ae33b4f9a",
+    "url": "/Royale-with-Cheese/static/css/1.0a5d370b.chunk.css"
+  },
+  {
+    "revision": "68a43dc7798ae33b4f9a",
+    "url": "/Royale-with-Cheese/static/js/1.68a43dc7.chunk.js"
+  },
+  {
+    "revision": "c42d55577fafe747eda6",
+    "url": "/Royale-with-Cheese/static/js/runtime~main.c42d5557.js"
+  },
+  {
+    "revision": "b2893f4ede9e946e1ab15e45439fc1a7",
+    "url": "/Royale-with-Cheese/index.html"
+  }
+];
